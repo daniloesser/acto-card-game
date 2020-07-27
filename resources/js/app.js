8a2a53d65b3032db90456aba17af070af35ec03f
Danilo Esser <daniloesser@gmail.com>
2020-07-27 19:36:57 -0400

    Set up Laravel's laravel/ui package to quick  scaffold all of the routes and views for authentication

---
+++ resources/js/app.js
@@ -0,0 +1,60 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+/**
+ * Create an "Event bus" to send and listen to events from different components in an easier way.
+ *
+ */
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+}
+
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+
+/**
+ * Register Card Game main components
+ */
+Vue.component('game-component', require('./components/GameComponent').default);
+Vue.component('leaderboard-component', require('./components/LeaderboardComponent').default);
+
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-4">
+        <main class="py-4">
+            <!--News card-->
+            <div class="card border-primary mb-4 text-center hoverable">
+                <div class="card-body">
+                    <!--Grid row-->
+                    <div class="row">
+
+                        <!--Grid column-->
+                        <div class="col-md-4 offset-md-1 mx-3 my-3">
+                            <!--Featured image-->
+                            <div class="view overlay hm-white-slight" style="padding-top: 50px;">
+                                <img
+                                    src="https://mma.prnewswire.com/media/996465/ACTO_Technologies__Inc__ACTO_and_Teleflex_Collaboration_Leads_to.jpg"
+                                    class="img-fluid" alt="ACTO LOGO">
+                                <a>
+                                    <div class="mask"></div>
+                                </a>
+                            </div>
+                        </div>
+                        <!--Grid column-->
+
+                        <!--Grid column-->
+                        <div class="col-md-7 text-left ml-3 mt-3">
+                            <!--Excerpt-->
+                            <a href="" class="green-text">
+                                <h6 class="font-bold pb-1"><i class="fa fa-desktop"></i> ACTO</h6>
+                            </a>
+                            <h4 class="mb-4"><strong>Take Home Assignment</strong></h4>
+                            <p> A Simple card game created with great technologies:</p>
+                            <ul>
+                                <li><i>Laradock</i></li>
+                                <li><i>Laravel</i></li>
+                                <li><i>Passport</i></li>
+                                <li><i>thephpleague/fractal</i></li>
+                                <li><i>VueJs</i></li>
+                                <li><i>Codeception</i></li>
+                            </ul>
+                            <p> by <a><strong>Danilo Esser</strong></a>, <i>July 2020</i><br></p>
+                            <a class="btn btn-lg btn-success offset-3" href="{{ route('login') }}"> Start playing</a>
+                        </div>
+                        <!--Grid column-->
+                    </div>
+                    <!--Grid row-->
+                </div>
+            </div>
+            <!--News card-->
+        </main>
+    </div>
+</div>
+</body>
+</html>
